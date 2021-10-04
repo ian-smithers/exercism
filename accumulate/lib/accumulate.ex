908964812139f444bf8584fbf33b985814830c2b
Ian Smithers <iansmithers@Ians-MacBook-Pro.local>
2021-10-04 12:33:23 +1100

    Accumulate exercise.

---
+++ accumulate/lib/accumulate.ex
@@ -0,0 +1,28 @@
+defmodule Accumulate do
+  @doc """
+    Given a list and a function, apply the function to each list item and
+    replace it with the function's return value.
+
+    Returns a list.
+
+    ## Examples
+
+      iex> Accumulate.accumulate([], fn(x) -> x * 2 end)
+      []
+
+      iex> Accumulate.accumulate([1, 2, 3], fn(x) -> x * 2 end)
+      [2, 4, 6]
+
+  """
+
+# def accumulate([], _), do: []
+  # def accumulate(list, fun) do
+  #   [head | tail] = list
+  #   [fun.(head) | accumulate(tail, fun)]
+  # end
+
+  @spec accumulate(list, (any -> any)) :: list
+  def accumulate(list, fun) do
+    for x <- list, do: fun.(x)
+  end
+end
